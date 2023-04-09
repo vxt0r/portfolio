55bf0230765a2de9fd7b91187d8200e0db35f5a8
Vitor <vitormoroli@gmail.com>
2023-04-09 18:33:57 -0300

    portfolio pronto

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-const main = document.querySelector('main')
-const ul = document.querySelector('ul')
+const btn = document.querySelector('.btn-menu')
+const opcoes = document.querySelector('.mobile-opcoes')
+
+btn.addEventListener('click', e =>{
+  const btnTexto = btn.innerText
+
+  if(btnTexto === 'Show'){
+    btn.innerText  = 'Hide'
+    opcoes.style.display = 'flex'
+
+  }
+
+  else if (btnTexto === 'Hide') {
+    btn.innerText  = 'Show'
+    opcoes.style.display = 'none'
+  }
+
+})
---
+++ readMe.md
@@ -0,0 +1,1 @@
+- Código do css pode ser refatorado
